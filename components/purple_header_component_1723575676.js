0dee5f1e87d8ccaa3be553fb6d2072c01f053bf5
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-13 19:05:42 +0000

    Code generation

--- components/purple_header_component_1723575676.js
+++ components/purple_header_component_1723575676.js
@@ -15,25 +15,25 @@ Vue.component("purple_header_component_1723575676", {
                 <nav id="desktop-navigation" class="hidden md:flex md:grow">
     
                     <ul id="desktop-menu-links" class="flex grow justify-center flex-wrap items-center">
-                        <li><a id="Trade-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" :class="[textStylePrimary, hoverTextStylePrimary]" href="Trade.html">Trade</a></li>
-                        <li><a id="Inventory-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Inventory.html" :class="[textStylePrimary, hoverTextStylePrimary]">Inventory</a></li>
-                        <li><a id="Market-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Market.html" :class="[textStylePrimary, hoverTextStylePrimary]">Market</a></li>
-                        <li><a id="Community-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Community.html" :class="[textStylePrimary, hoverTextStylePrimary]">Community</a></li>
-                        <li><a id="Account-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Account.html" :class="[textStylePrimary, hoverTextStylePrimary]">Account</a></li>
+                        <li><a id="Trade-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-white hover:text-blue-300" href="Trade.html">Trade</a></li>
+                        <li><a id="Inventory-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-white hover:text-blue-300" href="Inventory.html">Inventory</a></li>
+                        <li><a id="Market-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-white hover:text-blue-300" href="Market.html">Market</a></li>
+                        <li><a id="Community-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-white hover:text-blue-300" href="Community.html">Community</a></li>
+                        <li><a id="Account-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out text-white hover:text-blue-300" href="Account.html">Account</a></li>
                     </ul>
     
                 </nav>
     
                 <ul id="desktop-sign-in-links" class="flex-1 flex justify-end items-center">
-                    <li><a id="sign-in-link" class="font-medium text-sm whitespace-nowrap transition duration-150 ease-in-out" href="signin.html" :class="[textStylePrimary, hoverTextStylePrimary]">Sign in</a></li>
-                    <li class="ml-6"><a id="sign-up-link" class="btn-sm transition duration-150 ease-in-out w-full group relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none" href="signup.html" :class="[signUpButtonText, signUpButtonHover, signUpButtonGradient]"><span class="relative inline-flex items-center">Sign up <span id="sign-up-arrow" class="tracking-normal group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1" :class="signUpButtonArrow">-&gt;</span></span></a></li>
+                    <li><a id="sign-in-link" class="font-medium text-sm whitespace-nowrap transition duration-150 ease-in-out text-white hover:text-blue-300" href="signin.html">Sign in</a></li>
+                    <li class="ml-6"><a id="sign-up-link" class="btn-sm transition duration-150 ease-in-out w-full group relative before:absolute before:inset-0 before:bg-blue-800/30 before:rounded-full before:pointer-events-none bg-gradient-to-r from-blue-500 to-blue-700 text-white hover:from-blue-600 hover:to-blue-800" href="signup.html"><span class="relative inline-flex items-center">Sign up <span id="sign-up-arrow" class="tracking-normal group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1 text-blue-300">-&gt;</span></span></a></li>
                 </ul>
     
                 <div id="mobile-menu" class="md:hidden flex items-center ml-4" x-data="{ expanded: false }">
     
                     <button id="hamburger-button" class="hamburger" :class="{ 'active': expanded }" @click.stop="expanded = !expanded" aria-controls="mobile-nav" :aria-expanded="expanded">
                         <span class="sr-only">Menu</span>
-                        <svg id="hamburger-icon" class="w-5 h-5 fill-current transition duration-150 ease-in-out" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" :class="[textStylePrimary,hoverTextStylePrimary]">
+                        <svg id="hamburger-icon" class="w-5 h-5 fill-current transition duration-150 ease-in-out text-white hover:text-blue-300" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <rect y="2" width="20" height="2" rx="1" />
                             <rect y="9" width="20" height="2" rx="1" />
                             <rect y="16" width="20" height="2" rx="1" />
@@ -41,12 +41,12 @@ Vue.component("purple_header_component_1723575676", {
                     </button>
     
                     <nav id="mobile-nav" class="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" x-ref="mobileNav" :style="expanded ? 'max-height: ' + $refs.mobileNav.scrollHeight + 'px; opacity: 1' : 'max-height: 0; opacity: .8'" @click.outside="expanded = false" @keydown.escape.window="expanded = false" x-cloak>
-                        <ul id="mobile-nav-list" class="border border-transparent rounded-lg px-4 py-1.5" :class="signUpButtonGradient">
-                            <li><a id="mobile-Trade-link" class="flex font-medium text-sm py-1.5" href="Trade.html" :class="[textStylePrimary,hoverTextStylePrimary]">Trade</a></li>
-                            <li><a id="mobile-Inventory-link" class="flex font-medium text-sm py-1.5" href="Inventory.html" :class="[textStylePrimary,hoverTextStylePrimary]">Inventory</a></li>
-                            <li><a id="mobile-Market-link" class="flex font-medium text-sm py-1.5" href="Market.html" :class="[textStylePrimary,hoverTextStylePrimary]">Market</a></li>
-                            <li><a id="mobile-Community-link" class="flex font-medium text-sm py-1.5" href="Community.html" :class="[textStylePrimary,hoverTextStylePrimary]">Community</a></li>
-                            <li><a id="mobile-Account-link" class="flex font-medium text-sm py-1.5" href="Account.html" :class="[textStylePrimary,hoverTextStylePrimary]">Account</a></li>
+                        <ul id="mobile-nav-list" class="border border-transparent rounded-lg px-4 py-1.5 bg-gradient-to-r from-blue-800 to-blue-900">
+                            <li><a id="mobile-Trade-link" class="flex font-medium text-sm py-1.5 text-white hover:text-blue-300" href="Trade.html">Trade</a></li>
+                            <li><a id="mobile-Inventory-link" class="flex font-medium text-sm py-1.5 text-white hover:text-blue-300" href="Inventory.html">Inventory</a></li>
+                            <li><a id="mobile-Market-link" class="flex font-medium text-sm py-1.5 text-white hover:text-blue-300" href="Market.html">Market</a></li>
+                            <li><a id="mobile-Community-link" class="flex font-medium text-sm py-1.5 text-white hover:text-blue-300" href="Community.html">Community</a></li>
+                            <li><a id="mobile-Account-link" class="flex font-medium text-sm py-1.5 text-white hover:text-blue-300" href="Account.html">Account</a></li>
                         </ul>
                     </nav>
     
